039545f3a8a2ded009b67bdc0cab5c876323eda3
Stefan Haller <stefan@haller-berlin.de>
2024-06-13 16:55:20 +0200

    Show original todo action instead of "conflict", and show `<-- CONFLICT` instead
    It is useful to see if the conflicted commit was a "pick" or an "edit". What's
    more, we're about to add support for showing cherry-picks and reverts, and
    seeing that a conflicted commit was a revert is important because its diff is
    backwards compared to the diff of the conflicting files in the Files panel.

--- pkg/gui/controllers/local_commits_controller.go
+++ pkg/gui/controllers/local_commits_controller.go
@@ -1388,7 +1388,7 @@ func (self *LocalCommitsController) canMoveDown(selectedCommits []*models.Commit
 	if self.isRebasing() {
 		commits := self.c.Model().Commits
 
-		if !commits[endIdx+1].IsTODO() || commits[endIdx+1].Action == models.ActionConflict {
+		if !commits[endIdx+1].IsTODO() || commits[endIdx+1].Status == models.StatusConflicted {
 			return &types.DisabledReason{Text: self.c.Tr.CannotMoveAnyFurther}
 		}
 	}
@@ -1404,7 +1404,7 @@ func (self *LocalCommitsController) canMoveUp(selectedCommits []*models.Commit, 
 	if self.isRebasing() {
 		commits := self.c.Model().Commits
 
-		if !commits[startIdx-1].IsTODO() || commits[startIdx-1].Action == models.ActionConflict {
+		if !commits[startIdx-1].IsTODO() || commits[startIdx-1].Status == models.StatusConflicted {
 			return &types.DisabledReason{Text: self.c.Tr.CannotMoveAnyFurther}
 		}
 	}
